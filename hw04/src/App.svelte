3e0772ebc0af9fb6c6f8d46f5d386c6418b7b30d
Mohammad Shahin <59062739+MohammadShahin@users.noreply.github.com>
2022-10-05 14:59:22 +0300

    added the 4th homework (svelte)

---
+++ hw04/src/App.svelte
@@ -0,0 +1,28 @@
+<script land="ts">
+  // import Counter from "./lib/Counter.svelte";
+  import Header from "./lib/header.svelte";
+  import Education from "./lib/education.svelte";
+  import TechSkills from "./lib/techSkills.svelte";
+  import Animation from "./lib/animation.svelte";
+  import SolPrice from "./lib/solPrice.svelte";
+</script>
+
+<main>
+  <div class="container">
+    <Header />
+    <Education />
+    <TechSkills />
+    <Animation />
+    <SolPrice />
+  </div>
+</main>
+
+<style>
+  .container {
+    width: 50vw;
+    margin: auto;
+    padding: 20px;
+    border-radius: 20px;
+    background-color: white;
+  }
+</style>
---
+++ hw04/src/lib/techSkills.svelte
@@ -0,0 +1,15 @@
+<div id="tech-skills" class="main-section">
+    <div class="heading2">Technical Skills</div>
+    <p>
+        <span class="bold blue">Languages:</span> Python, Solidity, JavaScript, Typescript
+        Java, C++, C, C\#
+    </p>
+    <p>
+        <span class="bold blue">Frameworks/Libraries:</span> ReactJS, Ethers.js,
+        Redux, Numpy, Pandas, Flask, REST-API
+    </p>
+    <p>
+        <span class="bold blue">Developer Tools:</span> Git, VS Code, Docker, Jira,
+        Notion
+    </p>
+</div>
